4a275dbd72975053ceacd33254846d25fca42ede
Antonio Sitoe <antoniositoehl@gmail.com>
2025-08-22 08:30:03 +0200

    integrate redirect button

--- src/app/components/item-certificate/item-certificate.ts
+++ src/app/components/item-certificate/item-certificate.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ZardCardComponent } from '../card/card.component';
 import { ZardButtonComponent } from '../button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificate',
@@ -9,4 +10,10 @@ import { ZardButtonComponent } from '../button/button.component';
 })
 export class ItemCertificate {
   title: string | undefined;
+  id: string | undefined;
+  constructor(private router: Router) {}
+
+  gotoCertificate() {
+    this.router.navigate(['/certificate', this.id]);
+  }
 }
